309fc618d4b792ac84a7a1ce6980ea61958b1cd0
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-20 05:57:46 +0700

    update code

--- Task1/ManipulateArrOfObject.js
+++ Task1/ManipulateArrOfObject.js
@@ -1,24 +1,45 @@
 /* Manipulate Array of Object */
 
+let heroes = [
+    {
+        name : 'Jenderal Anumerta Ahmad Yani',
+        placeOfBirth : 'Purworejo',
+        yearOfBirth : 1922,
+        age : 43
+    },
+    {
+        name : 'Haji Agus Salim',
+        placeOfBirth : 'Agam, Sumatera Barat',
+        yearOfBirth : 1884,
+        age : 70
+    },
+    {
+        name : 'Pangeran Diponegoro',
+        placeOfBirth : 'Yogyakarta',
+        yearOfBirth : 1922,
+        age : 43
+    },
+     
+]
+
 /* Show all item on Array */
-const Fruits1 = ['Apple', 'Orange', 'Avocado', 'Banana', 'Guava', 'Coconut', 'Date'];
-console.log(Fruits1); // ['Apple', 'Orange', 'Avocado', 'Banana', 'Guava', 'Coconut', 'Date']
+console.log(heroes)
 
 /* Push item to Array */
-const Fruits2 = ['Apple', 'Orange'];
-const newFruits2 = 'Pear';
-Fruits2.push(newFruits2);
-console.log(Fruits2); // ['Apple', 'Orange', 'Pear']
+let newHero = {
+    name : 'Tuanku Imam Bonjol',
+    placeOfBirth : 'Sumatera Barat',
+    yearOfBirth : 1772,
+    age : 92
+}
+heroes.push(newHero)
 
 /* Delete item from Array */
-const Fruits3 = ['Apple', 'Avocado'];
-Fruits3.pop();
-console.log(Fruits3); // ['Apple']
+let removed = heroes.splice(1,2)
 
 /* Change value on Item property */
-let Fruits4 = ['Avocado', 'Pine', 'Melon'];
-Fruits4[2] = 'Watermelon';
-console.log(Fruits4); // [ 'Avocado', 'Pine', 'Watermelon' ]
+heroes[0].name = 'Kyai Haji Agus Salim'
+
 
 
 
