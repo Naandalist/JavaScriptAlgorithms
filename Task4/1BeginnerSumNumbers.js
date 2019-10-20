60332af4e30312fe332d7f0155a8940a91bdf3cf
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-21 05:24:05 +0700

    make it tidy

--- Task4/1BeginnerSumNumbers.js
+++ Task4/1BeginnerSumNumbers.js
@@ -2,12 +2,15 @@
 //Sum all Numbers
 
 /*CASE:
-Sebuah program memiliki fungsi untuk membaca array of numbers dan menjumlahkan seluruh member dari array tersebut sehingga hasilnya hanya berupa satu digit saja. Buatlah function untuk merealisasikan program tersebut.
+Sebuah program memiliki fungsi untuk membaca array of numbers
+dan menjumlahkan seluruh member dari array tersebut
+sehingga hasilnya hanya berupa satu digit saja.
+Buatlah function untuk merealisasikan program tersebut.
 Contoh INPUT : [123, 9, 27]
 
 Contoh OUTPUT: 6
 
-Proses Function: 
+Proses Function:
 123 + 9 + 27 = 159
 1 + 5 + 9 = 15
 1 + 5 = 6   */
