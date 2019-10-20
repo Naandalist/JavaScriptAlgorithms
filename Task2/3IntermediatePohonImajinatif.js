425b51132468878369405d7da4a69c008a03c295
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-20 09:25:50 +0700

    imaginative tree

---
+++ Task2/3IntermediatePohonImajinatif.js
@@ -0,0 +1,23 @@
+//Kategori: Intermediate
+//Pohon Imajinatif
+
+/* CASE:
+Diketahui sebuah pohon imajinatif yang tumbuh di sebuah taman.
+Pohon tersebut hanya tumbuh sepanjang 1 m saat musim gugur dan 2 kali lipat tinggi pohon tersebut saat musim semi.
+Buatlah program yang menentukan tinggi pohon tersebut setelah Y tahun dengan tinggi awal X m.
+
+REQUIREMENT:
+Input user berupa nilai X dan Y dengan ketentuan :
+X > 0
+Y > 0
+Output berupa tinggi akhir pohon tersebut dengan ketentuan:
+Pada 1 tahun, musim semi datang terlebih dahulu dibandingkan musim gugur.   */
+
+const imaginativeTree = (x, y) => {
+    if (x < 0 || y < 0){
+        return `Numbers below 0 are not allowed`
+    }
+
+    return `${((2 * x) + 1) * y } meter`
+}
+console.log(imaginativeTree(2, 1))
