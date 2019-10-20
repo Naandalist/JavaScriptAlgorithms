ce5668ea3096ecbc1d31c5814feb46c928503416
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-20 07:54:20 +0700

    updatefile

--- Task2/2BeginnerSegitigaSikuSiku.js
+++ Task2/2BeginnerSegitigaSikuSiku.js
@@ -23,16 +23,47 @@ OUTPUT :
 2 3 5 7
 2 3 5 7 11  */
 
-function generatePyramid() {
-    var totalNumberofRows = 5;
-    var output="";
-    for (var i = 1; i <= totalNumberofRows; i++) {
-        for (var j = 1; j <= i; j++) {
-            output+=j;
-        }
-        console.log(output);
-        output="";
-    }
-}  
+function getPrime(num) {
 
-console.log(generatePyramid())
+    let prime=[];
+    for(let loop=2;loop<=num+10;loop++) {
+    let store=0
+
+    //when the loop is below 10 and above 2
+    if(loop<=9 && loop>=2) {
+      for(let i=2;i<=9;i++){
+        if(loop%i===0) {
+          store+=1;
+        }
+      }
+      if(store===1) {
+      prime.push(loop);
+      }
+    }
+    
+    //when the loop is above 9
+    if(loop>=10) {
+        for(let j=2;j<=9;j++){
+        if(loop%j===0) {
+          store+=1;
+        }
+      }
+        if(store===0) {
+        prime.push(loop);
+      }
+    }
+    
+   }
+    
+  //create a half pyramyd
+  let result=''
+  for (let k=0; k <= num; k++){
+    for (let l=0; l < k; l++){
+        result +=prime[l]+' '
+    }
+    console.log(result)
+    result=''
+  }
+}
+
+getPrime(5);
