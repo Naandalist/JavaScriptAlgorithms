3cd3e4b6bdf2fd97dd3861a24bef4d0e7b5753b6
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-21 05:21:09 +0700

    add library app on task3

---
+++ Task3/4IntermediateAplikasiPerpustakaan.js
@@ -0,0 +1,70 @@
+//Intermediate
+//library application
+
+/*CASE:
+Sebuah perpustakaan menyimpan data buku-bukunya dalam array of object(key title, author, status).
+Buatlah sebuah program yang dapat menampilkan output berdasarkan pilihan:
+
+    a. Menampilkan status sebuah buku jika diberikan parameter judul buku tersebut.
+    b. Menampilkan list buku yang tersedia untuk dipinjam.  */
+
+
+
+const checkBookStatus= (bookName) => {
+    const lib= [
+        {
+          title: 'Snow Crash',
+          author: 'Neal Stephenson',
+          status: 'available'
+
+        },
+        {
+          title: 'Stranger in a Strange Land',
+          author: 'Robert Heinlein',
+          status: 'not available',
+        },
+        {
+          title: 'Cryptonomicon',
+          author: 'Neal Stephenson',
+          status: 'available'
+        }
+      ]
+
+      for (let i=0; i< lib.length; i++){
+        if (lib[i].title === bookName){
+          return lib[i].status
+        }
+      }
+}
+    console.log(checkBookStatus('Stranger in a Strange Land'))
+
+const availableBookList = (dreamStatus) => {
+
+    const lib= [
+        {
+          title: 'Snow Crash',
+          author: 'Neal Stephenson',
+          status: 'available'
+        },
+        {
+          title: 'Stranger in a Strange Land',
+          author: 'Robert Heinlein',
+          status: 'not available',
+        },
+        {
+          title: 'Cryptonomicon',
+          author: 'Neal Stephenson',
+          status: 'available'
+        }
+      ]
+
+    const availableBook= []
+    for (let i=0; i< lib.length; i++){
+        if (lib[i].status === dreamStatus){
+            availableBook.push(lib[i].title)
+        }
+    }
+    return availableBook.join(', ')
+
+}
+console.log(availableBookList('available'))
