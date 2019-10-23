98ed2e7cfeed7fd8fc7d8c19d8a0605ef970f739
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-23 21:26:46 +0700

    update on task3

--- Task3/3AdvancedMultiplyMatrix2D.js
+++ Task3/3AdvancedMultiplyMatrix2D.js
@@ -1,30 +1,35 @@
-//
-//
+//Kategori: Intermediate
+//Multiply matrix
+
 /* CASE:
-Buatlah program yang memiliki parameter sebuah matriks(array 2 dimensi)
+Buatlah program yang memiliki parameter sebuah matriks (array 2 dimensi)
 dan memiliki return berupa transpose dari matriks tersebut.
 */
 
-  const multiplyMatrix2D= (a, b) => {
-    let aNumRows = a.length, aNumCols = a[0].length
-    let bNumRows = b.length, bNumCols = b[0].length
-    let matrix = []  // initialize array of rows
 
-    //if () return 'it cant multiply'
+  const multiplyMatrix2D= (m1, m2) => {
 
-    for (let r = 0; r < aNumRows; ++r) {
-      matrix[r] = [];                 // initialize the current row
+    if (m1.length !== 2 || m2.length !==2) return false
 
-      for (let c = 0; c < bNumCols; ++c) {
-        matrix[r][c] = 0;             // initialize the current cell
+    let matrix = []
+    for (let i = 0; i < m1.length; i++) {
+      matrix[i] = [];
 
-        for (let i = 0; i < aNumCols; ++i) {
-          matrix[r][c] += a[r][i] * b[i][c];
+      for (let  j= 0; j < m2[0].length; j++) {
+        matrix[i][j] = 0;
+
+        for (let k = 0; k < m1[0].length; k++) {
+
+          if (m2[k][j] === undefined || m1[i][k] === undefined) return false
+
+          matrix[i][j] += m1[i][k] * m2[k][j];
+
         }
       }
     }
     return matrix;
   }
+
 
 
   let a = [[3, 4],
