93ce2d741b7daca08c34cb3ae8bc1919dca088aa
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-24 10:25:19 +0700

    add indent

--- Task0/ManipulateArrOfObject.js
+++ Task0/ManipulateArrOfObject.js
@@ -1,41 +1,41 @@
 /* Manipulate Array of Object */
 
-let heroes = [
-    {
-        name : '👮 Jenderal Anumerta Ahmad Yani',
-        placeOfBirth : 'Purworejo',
-        yearOfBirth : 1922,
-        age : 43
-    },
-    {
-        name : '👳‍ Haji Agus Salim',
-        placeOfBirth : 'Agam, Sumatera Barat',
-        yearOfBirth : 1884,
-        age : 70
-    },
-    {
-        name : '🤴 Pangeran Diponegoro',
-        placeOfBirth : 'Yogyakarta',
-        yearOfBirth : 1922,
-        age : 43
-    },
+    let heroes = [
+        {
+            name : '👮 Jenderal Anumerta Ahmad Yani',
+            placeOfBirth : 'Purworejo',
+            yearOfBirth : 1922,
+            age : 43
+        },
+        {
+            name : '👳‍ Haji Agus Salim',
+            placeOfBirth : 'Agam, Sumatera Barat',
+            yearOfBirth : 1884,
+            age : 70
+        },
+        {
+            name : '🤴 Pangeran Diponegoro',
+            placeOfBirth : 'Yogyakarta',
+            yearOfBirth : 1922,
+            age : 43
+        },
 
-]
+    ]
 
-/* Show all item on Array */
-console.table(heroes)
+    /* Show all item on Array */
+    console.table(heroes)
 
-/* Push item to Array */
-let newHero = {
-    name : '👳‍ Tuanku Imam Bonjol',
-    placeOfBirth : 'Sumatera Barat',
-    yearOfBirth : 1772,
-    age : 92
-}
-heroes.push(newHero)
+    /* Push item to Array */
+    let newHero = {
+        name : '👳‍ Tuanku Imam Bonjol',
+        placeOfBirth : 'Sumatera Barat',
+        yearOfBirth : 1772,
+        age : 92
+    }
+    heroes.push(newHero)
 
-/* Delete item from Array */
-let removed = heroes.pop()
+    /* Delete item from Array */
+    let removed = heroes.pop()
 
-/* Change value on Item property */
-heroes[0].name = '👳‍ Kyai Haji Agus Salim'
+    /* Change value on Item property */
+    heroes[0].name = '👳‍ Kyai Haji Agus Salim'
